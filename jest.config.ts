ac19b2cad4d05d369faa3a198bae4299e94520ba
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-06-05 19:10:45 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+
+
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+};
