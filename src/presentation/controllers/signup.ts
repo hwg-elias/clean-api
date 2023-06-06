274d8c73e635841ac9e9933824212ecfa733440b
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-06-06 16:36:17 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,11 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error('Missing param: name')
-
       }
     }
 
@@ -12,8 +13,12 @@ export class SignUpController {
       return {
         statusCode: 400,
         body: new Error('Missing param: email')
-
       }
+    }
+
+    return {
+      statusCode: 200,
+      body: 'Success'
     }
   }
 }
