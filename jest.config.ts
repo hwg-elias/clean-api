0438df64ab09a02b3bd42e48e9a6de96a6de0309
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-06-08 16:46:44 -0300

    fix: eslint bug fix

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,10 @@
 
-
 export default {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-};
+  }
+}
