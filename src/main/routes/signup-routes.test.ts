4709ab353dacce22d87afe9f6e179801be2b3f6c
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-07-04 17:03:53 -0300

    test: ensure SignUp route return an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../config/app'
+describe('CORS Middleware', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Yasmin Paulina',
+        email: 'paulina.yasminica@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
+      .expect(200)
+  })
+})
