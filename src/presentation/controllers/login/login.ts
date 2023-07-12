2be9b47dcedc59c407bc9cb7443def21a0a9a7d0
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-07-12 09:57:21 -0300

    feat: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,14 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
+  private readonly emailValidatorStub: EmailValidator
+  constructor (emailValidatorStub: EmailValidator) {
+    this.emailValidatorStub = emailValidatorStub
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
       return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))) })
@@ -11,5 +17,6 @@ export class LoginController implements Controller {
     if (!httpRequest.body.password) {
       return await new Promise(resolve => { resolve(badRequest(new MissingParamError('password'))) })
     }
+    this.emailValidatorStub.isValid(httpRequest.body.email)
   }
 }
