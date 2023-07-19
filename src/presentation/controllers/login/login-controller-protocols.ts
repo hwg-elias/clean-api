f6eda2c3405402709ab5bd80e152576af22ff59d
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-07-19 08:58:46 -0300

    feat: ensure SignUpController calls Authentication with correct values

--- src/presentation/controllers/login/login-controller-protocols.ts
+++ src/presentation/controllers/login/login-controller-protocols.ts
@@ -1,3 +1,3 @@
 export * from '../../protocols'
-export * from '../../../domain/usecases/authentication'
 export * from '../../protocols/validation'
+export * from '../../../domain/usecases/authentication'
--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,10 +1,11 @@
-import { HttpRequest, HttpResponse, Controller, AddAccount, Validation } from './signup-controller-protocols'
+import { HttpRequest, HttpResponse, Controller, AddAccount, Validation, Authentication } from './signup-controller-protocols'
 import { badRequest, serverError, ok } from '../../helpers/http/http-helper'
 
 export class SignUpController implements Controller {
   constructor (
     private readonly addAccount: AddAccount,
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -17,6 +18,10 @@ export class SignUpController implements Controller {
 
       const account = await this.addAccount.add({
         name,
+        email,
+        password
+      })
+      await this.authentication.auth({
         email,
         password
       })
