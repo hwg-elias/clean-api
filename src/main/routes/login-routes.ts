64cf43ba324fae442f098a37d33a71e6ed9ed4ed
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-07-19 11:42:41 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-routes-adapter'
-import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
 import { makeLoginController } from '../factories/controllers/login/login-controller-factory'
+import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
