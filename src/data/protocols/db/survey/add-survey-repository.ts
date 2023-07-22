c0ff973b8139fd2768726b8aad4328888223c193
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-07-21 21:21:46 -0300

    feat: ensure DbAddSurvey call DbAddSurveyRepository with correct values

---
+++ src/data/protocols/db/survey/add-survey-repository.ts
@@ -0,0 +1,6 @@
+
+import { AddSurveyModel } from 'domain/usecases/add-survey'
+
+export class AddSurveyRepository {
+  async add (surveyData: AddSurveyModel): Promise<void> {}
+}
---
+++ src/data/usecases/add-survey/db-add-survey.ts
@@ -0,0 +1,12 @@
+import { AddSurvey, AddSurveyModel, AddSurveyRepository } from './db-add-survey-protocols'
+
+export class DbAddSurvey implements AddSurvey {
+  constructor (
+    private readonly addSurveyRepository: AddSurveyRepository
+  ) {}
+
+  async add (data: AddSurveyModel): Promise<void> {
+    await this.addSurveyRepository.add(data)
+    return await new Promise(resolve => resolve())
+  }
+}
