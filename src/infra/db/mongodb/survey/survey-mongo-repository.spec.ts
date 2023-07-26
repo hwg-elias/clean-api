93823ec723ed171a6a51f11cb079133e77453da5
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-07-26 17:04:58 -0300

    refactor: organize SurveyMongoRepository tests in describes

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -5,6 +5,24 @@ import env from '../../../../main/config/env'
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
 
 let surveyColletion: Collection
+
+const makeFakeSurveyData = (): AddSurveyModel => ({
+  question: 'any_question',
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer'
+    },
+    {
+      answer: 'other_answer'
+    }
+  ],
+  date: new Date()
+})
+
+const makeSut = (): SurveyMongoRepository => {
+  return new SurveyMongoRepository()
+}
 describe('Survey Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(env.mongoUrl)
@@ -17,27 +35,12 @@ describe('Survey Mongo Repository', () => {
     await surveyColletion.deleteMany({})
   })
 
-  const makeFakeSurveyData = (): AddSurveyModel => ({
-    question: 'any_question',
-    answers: [
-      {
-        image: 'any_image',
-        answer: 'any_answer'
-      },
-      {
-        answer: 'other_answer'
-      }
-    ],
-    date: new Date()
-  })
-
-  const makeSut = (): SurveyMongoRepository => {
-    return new SurveyMongoRepository()
-  }
-  test('Should add a survey on success', async () => {
-    const sut = makeSut()
-    await sut.add(makeFakeSurveyData())
-    const survey = await surveyColletion.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+  describe('add()', () => {
+    test('Should add a survey on success', async () => {
+      const sut = makeSut()
+      await sut.add(makeFakeSurveyData())
+      const survey = await surveyColletion.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
+    })
   })
 })
