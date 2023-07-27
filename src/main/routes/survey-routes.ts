49fccee155e915e884f786daa32f8aded0972fc4
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-07-26 22:42:39 -0300

    feat: ensure SurveyRoutes return 403 on GET routes with no token provided

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,9 +3,11 @@ import { adaptRoute } from '../adapters/express-routes-adapter'
 import { makeAddSurveyController } from '../../main/factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { adaptMiddleware } from '../../main/adapters/express-middleware-adapter'
 import { makeAuthMiddlewares } from '../../main/factories/middlewares/auth-middleware-factory'
+import { makeLoadSurveysController } from '../../main/factories/controllers/survey/load-surveys/load-surveys-controller-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddlewares('admin'))
+  const auth = adaptMiddleware(makeAuthMiddlewares())
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
-  router.get('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }
