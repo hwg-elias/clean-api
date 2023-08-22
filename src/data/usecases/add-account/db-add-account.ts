2557915b87ee292947905ad62b57218f6a47e952
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-08-22 11:07:48 -0300

    fix: ensure code is readble

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -18,7 +18,9 @@ export class DbAddAccount implements AddAccount {
     const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
     if (!account) {
       const hashedPassword = await this.hasher.hash(accountData.password)
-      const newAccount = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+      const newAccount = await this.addAccountRepository.add(
+        Object.assign({}, accountData, { password: hashedPassword })
+      )
       return newAccount
     }
     return null
