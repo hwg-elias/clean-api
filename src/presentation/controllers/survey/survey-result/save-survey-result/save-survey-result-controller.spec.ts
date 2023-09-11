6caeaed1972dec15835cddd0e1f98039e56ca6ed
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2023-09-11 11:22:44 -0300

    test: ensure SaveSurveyResultController returns 200 on success

--- src/presentation/controllers/survey/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -4,7 +4,7 @@ import {
   HttpRequest, SurveyModel, LoadSurveyById,
   SaveSurveyResult, SaveSurveyResultModel, SurveyResultModel
 } from './save-survey-result-controller-protocols'
-import { forbidden, serverError } from '@app/presentation/helpers/http/http-helper'
+import { forbidden, ok, serverError } from '@app/presentation/helpers/http/http-helper'
 import MockDate from 'mockdate'
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -131,5 +131,11 @@ describe('SaveSurveyResult Controller', () => {
     jest.spyOn(saveSurveyResultStub, 'save').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok(makeFakeSurveyResult()))
   })
 })
